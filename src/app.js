b4ace2c46604e28f3efc363955654e5039954e4f
Carlos Blandino <cablandino66@gmail.com>
2021-03-24 12:54:07 -0700

    adding post to flips

--- src/app.js
+++ src/app.js
@@ -4,9 +4,7 @@ const app = express();
 // TODO: Follow instructions in the checkpoint to implement ths API.
 const counts = require("./data/count-data");
 const flips = require("./data/flip-data");
-
-
-
+app.use(express.json())
 
 app.use("/flips/:flipId", (req, res, next) => {
   const { flipId } = req.params;
@@ -34,10 +32,28 @@ app.use("/counts", (req, res) => {
   res.json({ data: counts });
 });
 
-app.use("/flips", (req, res)=>{
+app.get("/flips", (req, res)=>{
   res.json({data:flips});
 })
+// Variable to hold the next id.
+// Since some ID's may already be used, you find the largest assigned id.
+let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
 
+app.post("/flips", (req, res, next) => {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+  const newFlip = {
+    id: ++lastFlipId, // Increment last id then assign as the current ID
+    result,
+  };
+  flips.push(newFlip);
+  counts[result] = counts[result] + 1; // Increment the counts
+  //res.json({ data: newFlip });
+  res.status(201).json({ data: newFlip });
+  } else {
+    res.sendStatus(400);
+  }
+});
 
 // Not found handler
 app.use((request, response, next) => {
